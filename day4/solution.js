03ac2561e7ca12b82a698dcacdb135ab36bb4bd7
Murtiag <julian.christopher.eicher@gmail.com>
2023-12-12 11:04:32 +0100

    feat: day 4 part 2

--- day4/solution.js
+++ day4/solution.js
@@ -1,6 +1,6 @@
 import { open } from "node:fs/promises";
 
-part1();
+part2();
 
 async function part1() {
   const file = await open("./input.txt");
@@ -8,7 +8,6 @@ async function part1() {
   let output = 0;
 
   for await (const line of file.readLines()) {
-    console.log("=========================");
     const [left, right] = line.split("|");
     const winningNrs = left
       .split(":")[1]
@@ -28,3 +27,42 @@ async function part1() {
 
   console.log("Part 1 Result:", output);
 }
+
+async function part2() {
+  const file = await open("./input.txt");
+  let originalLines = [];
+
+  let output = 0;
+
+  for await (const line of file.readLines()) {
+    originalLines.push(line);
+  }
+
+  function iterateLines(lines) {
+    output += lines.length;
+    let new_array = [];
+
+    lines.forEach((line) => {
+      const [left, right] = line.split("|");
+      const cardNr = Number.parseInt(left.substring(4, 8).trim());
+      const winningNrs = left
+        .split(":")[1]
+        .split(" ")
+        .filter((nr) => nr != "");
+      const myNrs = right.split(" ").filter((nr) => nr != "");
+      const amountWon = winningNrs.filter((nr) => myNrs.includes(nr)).length;
+
+      // win any number of additional cards
+      for (let i = 0; i < amountWon; i++) {
+        if (cardNr + i <= originalLines.length)
+          new_array.push(originalLines[cardNr + i]);
+      }
+    });
+
+    if (new_array.length != 0) iterateLines(new_array);
+  }
+
+  iterateLines(originalLines);
+  console.log("Part 1 Result:", output);
+}
+  
